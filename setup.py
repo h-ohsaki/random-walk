b4b88b93214727af4f5ef926320f98ab705e75e4
Hiroyuki Ohsaki <ohsaki@lsnl.jp>
2023-03-20 22:23:39 +0900

    version 0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='randwalk',
-    version='0.1',
+    version='0.2',
     author='Hiroyuki Ohsaki',
     author_email='ohsaki@lsnl.jp',
     description='random walk agents on graph supporting several strategies',
